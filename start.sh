540b1799a165d4cb793e6629e10954a070507f46
badman99dev <badman993944@gmail.com>
2026-02-23 13:05:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # 1. Python सिंक स्क्रिप्ट को बैकग्राउंड में चलाओ
 python /sync.py &
 
-# 2. Meilisearch को फोरग्राउंड (सामने) चलाओ ताकि सर्वर चलता रहे
-meilisearch
+# 2. Meilisearch को पब्लिक IP (0.0.0.0) पर चलाओ ताकि Render उसे देख सके
+meilisearch --http-addr 0.0.0.0:7700
